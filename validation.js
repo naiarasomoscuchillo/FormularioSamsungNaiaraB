05df55e5a298ce6bea86a39c9dd1cd09ec07e2a0
naiarasomoscuchillo <naiara@somoscuchillo.com>
2022-02-20 21:59:53 +0100

    pequeño ajuste final

--- validation.js
+++ validation.js
@@ -1,5 +1,5 @@
-var provinceCodes = new Map ([ [01, "alava"], [02,"albacete"], [03,"alicante"], [04,"almeria"], [05,"avila"], [06,"badajoz"], [07,"islas baleares"], [08,"barcelona"],  [09,"burgos"], [10,"caceres"], [11,"cadiz"], [12,"castellon"],  [13,"ciudad real"], [14,"cordoba"], [15,"a coruñna"], [16,"cuenca"],  [17,"gerona"], [18,"granada"], [19,"guadalajara"], [20,"guipuzcoa"],  [21,"huelva"], [22,"huesca"], [23,"jaen"], [24,"leon"],  [25,"lleida"], [26,"la rioja"], [27,"lugo"], [28,"madrid"], [29,"malaga"], [30,"murcia"], [31,"navarra"], [32,"orense"], [33,"asturias"], [34,"palencia"], [35,"las palmas"], [36,"pontevedra"],  [37,"salamanca"], [38,"santa cruz de tenerife"], [39,"cantabria"], [40,"segovia"], [41,"sevilla"], [42,"soria"], [43,"tarragona"], [44,"teruel"], [45,"toledo"], [46,"valencia"], [47,"valladolid"], [48,"vizcaya"], [49,"zamora"], [50,"zaragoza"], [51,"ceuta"], [52,"melilla"] ]);
-var provinces = ["alava", "albacete", "alicante", "almeria", "avila", "badajoz","islas baleares", "barcelona", "burgos", "caceres", "cadiz", "castellon", "ciudad real", "cordoba", "a coruñna", "cuenca", "gerona", "granada", "guadalajara", "guipuzcoa", "huelva", "huesca", "jaen", "leon", "lleida", "la rioja", "lugo", "madrid", "malaga", "murcia", "navarra", "orense", "asturias", "palencia", "las palmas", "pontevedra" ,"salamanca", "santa cruz de tenerife", "cantabria", "segovia", "sevilla", "soria", "tarragona", "teruel", "toledo", "valencia", "valladolid", "vizcaya", "zamora", "zaragoza", "ceuta", "melilla"];
+var provinceCodes = new Map ([ [01, "alava"], [02,"albacete"], [03,"alicante"], [04,"almeria"], [05,"avila"], [06,"badajoz"], [07,"islas baleares"], [08,"barcelona"],  [09,"burgos"], [10,"caceres"], [11,"cadiz"], [12,"castellon"],  [13,"ciudad real"], [14,"cordoba"], [15,"a coruna"], [16,"cuenca"],  [17,"gerona"], [18,"granada"], [19,"guadalajara"], [20,"guipuzcoa"],  [21,"huelva"], [22,"huesca"], [23,"jaen"], [24,"leon"],  [25,"lleida"], [26,"la rioja"], [27,"lugo"], [28,"madrid"], [29,"malaga"], [30,"murcia"], [31,"navarra"], [32,"orense"], [33,"asturias"], [34,"palencia"], [35,"las palmas"], [36,"pontevedra"],  [37,"salamanca"], [38,"santa cruz de tenerife"], [39,"cantabria"], [40,"segovia"], [41,"sevilla"], [42,"soria"], [43,"tarragona"], [44,"teruel"], [45,"toledo"], [46,"valencia"], [47,"valladolid"], [48,"vizcaya"], [49,"zamora"], [50,"zaragoza"], [51,"ceuta"], [52,"melilla"] ]);
+var provinces = ["alava", "albacete", "alicante", "almeria", "avila", "badajoz","islas baleares", "barcelona", "burgos", "caceres", "cadiz", "castellon", "ciudad real", "cordoba", "a coruna", "cuenca", "gerona", "granada", "guadalajara", "guipuzcoa", "huelva", "huesca", "jaen", "leon", "lleida", "la rioja", "lugo", "madrid", "malaga", "murcia", "navarra", "orense", "asturias", "palencia", "las palmas", "pontevedra" ,"salamanca", "santa cruz de tenerife", "cantabria", "segovia", "sevilla", "soria", "tarragona", "teruel", "toledo", "valencia", "valladolid", "vizcaya", "zamora", "zaragoza", "ceuta", "melilla"];
 var week = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado'];
 
 var province = document.getElementById("provincia");
@@ -42,14 +42,12 @@ function validate(target) {
         }
     }
 
-    // 
     if (target.dataset.number !== undefined) {
 
         if (target.value.match(/^[0-9]+$/) === null) {
             valid = false;
         }
     }
-    // 
     
     if (valid) {
         target.classList.add('valid');
@@ -75,8 +73,6 @@ function main() {
         }
 
         return valid;
-        // if (!valid) {
-        // }
     });
 }
 
@@ -84,36 +80,25 @@ main();
 
 
 
-/*
+function confirm_reset() {
+    return confirm("¿Estás seguro de borrar todos los datos?");
+}
 
-//esto para que no me deje escribir caracteres no numéricos
-// Restricts input for the given textbox to the given inputFilter.
-function setInputFilter(textbox, inputFilter) {
-    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
-      textbox.addEventListener(event, function() {
-        if (inputFilter(this.value)) {
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-        } else if (this.hasOwnProperty("oldValue")) {
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        } else {
-          this.value = "";
-        }
-      });
-    });
-  }
-  
-// Install input filters.
-setInputFilter(document.getElementById("iban"), function(value) {
-    return /^-?\d*$/.test(value); });
-setInputFilter(document.getElementById("entidad"), function(value) {
-    return /^-?\d*$/.test(value); });
-setInputFilter(document.getElementById("sucursal"), function(value) {
-    return /^-?\d*$/.test(value); });
-setInputFilter(document.getElementById("dc"), function(value) {
-    return /^-?\d*$/.test(value); });
-setInputFilter(document.getElementById("cuenta"), function(value) {
-    return /^-?\d*$/.test(value); });
-*/
+
+function infoValoracion(){  
+    var puntos = document.getElementById("statistics__bar").value; 
+    alert('Has valorado con ' + puntos + ' puntos.');
+
+}  
+
+function cuentaBancaria(){  
+    var pais = document.getElementById("pais").value; 
+    var iban = document.getElementById("iban").value; 
+    var entidad = document.getElementById("entidad").value; 
+    var sucursal = document.getElementById("sucursal").value; 
+    var dc = document.getElementById("dc").value; 
+    var cuenta = document.getElementById("cuenta").value; 
+
+    alert('Le informamos de que su cuenta bancaria es ' + pais + '-' + iban + '-' + entidad + '-' + sucursal + '-' + dc + '-' + cuenta );
+
+}
